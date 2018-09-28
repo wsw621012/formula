8f76a19f10fbaf6c8304607593061fb1633b1aec
wsw621012 <siwei_wang@trendmicro.com>
2018-09-28 17:37:57 +0800

    try to find line to introduce car direction

--- image_test.py
+++ image_test.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import math
 
 def flatten_rgb(img):
     b, g, r = cv2.split(img)
@@ -36,11 +37,11 @@ def draw_line(inputImage):
     cv2.imshow("Trolley_Problem_Result", inputImage)
     cv2.imshow('edge', edges)
 
-
-img = cv2.imread("./Log/IMG/eagle_2018_09_27_17_30_16_851.jpg")
-crop_img = img[100:240, 0:320]
+#eagle_2018_09_28_16_22_14_445-> 1st: 140:240, p1 = 4, p2 = 80
+#eagle_2018_09_28_16_22_17_238
+img = cv2.imread("./Log/IMG/eagle_2018_09_28_16_55_37_684.jpg")
+crop_img = img[140:240, 0:320]
 flatten_img = flatten_rgb(crop_img)
-cv2.imshow("flattened", flatten_img)
 
 (B, G, R) = cv2.split(flatten_img)
 sky_filter = ((B == 255) & (G == 255) & (R == 255))
@@ -51,21 +52,45 @@ G[sky_filter] = 0
 zeros = np.zeros(flatten_img.shape[:2], dtype = "uint8")
 
 #sky_img = cv2.merge((B == 255), (G == 255), (R == 255))
+edge_g = cv2.Canny(G, 50, 150, apertureSize = 3)
+cv2.imshow('green', edge_g)
 
-g_img = cv2.merge([zeros, G, zeros])
-b_img = cv2.merge([B, zeros, zeros])
-r_img = cv2.merge([zeros, zeros, R])
+lines = cv2.HoughLines(edge_g, 4, np.pi / 180, 80, None, 0, 0)
 
-canny = cv2.Canny(B, 50, 150, apertureSize = 3)
-cv2.imshow('Canny-blue', canny)
-
-lines = cv2.HoughLines(canny, 5, np.pi / 180, 150, None, 0, 0)
-    
 if lines is not None:
     for i in range(0, len(lines)):
         rho = lines[i][0][0]
         theta = lines[i][0][1]
-        print("rho = %.2f, theta = %.2f" % (rho, theta * 180 / np.pi))
+        print("G:rho = %.2f, theta = %.2f" % (rho, theta * 180 / np.pi))
+        x1 = int(rho*math.cos(theta) - 1000*math.sin(theta))
+        y1 = int(rho*math.sin(theta) + 1000*math.cos(theta))
+        x2 = int(rho*math.cos(theta) + 1000*math.sin(theta))
+        y2 = int(rho*math.sin(theta) - 1000*math.cos(theta))
+        cv2.line(flatten_img, (x1, y1), (x2, y2), (255,255,255), 1)
+
+edge_r = cv2.Canny(R, 50, 150, apertureSize = 3)
+cv2.imshow('red', edge_g)
+
+lines = cv2.HoughLines(edge_r, 4, np.pi / 180, 80, None, 0, 0)
+
+if lines is not None:
+    for i in range(0, len(lines)):
+        rho = lines[i][0][0]
+        theta = lines[i][0][1]
+        print("R:rho = %.2f, theta = %.2f" % (rho, theta * 180 / np.pi))
+        x1 = int(rho*math.cos(theta) - 1000*math.sin(theta))
+        y1 = int(rho*math.sin(theta) + 1000*math.cos(theta))
+        x2 = int(rho*math.cos(theta) + 1000*math.sin(theta))
+        y2 = int(rho*math.sin(theta) - 1000*math.cos(theta))
+        cv2.line(flatten_img, (x1, y1), (x2, y2), (255,255,255), 1)
+
+
+cv2.imshow("flattened", flatten_img)
+
+#g_img = cv2.merge([zeros, G, zeros])
+#b_img = cv2.merge([B, zeros, zeros])
+#r_img = cv2.merge([zeros, zeros, R])
+
 #cv2.imshow("Sky", sky_img)
 #cv2.imshow("Blue", b_img)
 #cv2.imshow("Green", g_img)
